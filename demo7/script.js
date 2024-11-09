03b866bab58e34e423374ceedf3daa17f6b9d87d
Tomatos <3333331969@qq.com>
2024-11-09 22:13:40 +0800

    improve demo7

--- demo7/script.js
+++ demo7/script.js
@@ -1,53 +1,9 @@
-const email = document.querySelector('.in-email');
-const password = document.querySelector('.in-password');
-const time = 50;
+const labels = document.querySelectorAll('.login-item label');
 
-
-function offset(spans, patten) {
-    let n = spans.length;
-    let i = 0;
-    setInterval(() =>{
-        if(i < n) {
-            if(patten == 1) {
-                spans[i].classList.add('recovery');
-            } else {
-                spans[i].classList.remove('recovery');
-            }
-            ++i;
-        } else {
-            clearInterval();
-        }
-    }, time);
-};
-
-function checkContainText(input) {
-    console.log(input.value);
-    return input.value !== '';
-}
-
-
-
-email.addEventListener('focus', () => {
-    const spans = document.querySelectorAll('.email span');
-    offset(spans, 1);
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
 });
 
-email.addEventListener('blur', () => {
-    if(!checkContainText(email)) {
-        const spans = document.querySelectorAll('.email span');
-        offset(spans, 0);
-    }
-});
-
-
-password.addEventListener('focus', () => {
-    const spans = document.querySelectorAll('.password span');
-    offset(spans, 1);
-});
-
-password.addEventListener('blur', () => {
-    if(!checkContainText(password)) {
-        const spans = document.querySelectorAll('.password span');
-        offset(spans, 0);
-    }
-});
