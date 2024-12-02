e0bb787d43f83e3ce3ba235aff81db3c7e8d0436
Tomatos <3333331969@qq.com>
2024-12-02 17:14:15 +0800

    Improve Demo4

--- demo4/script.js
+++ demo4/script.js
@@ -1,70 +1,27 @@
-const btns = document.querySelectorAll('button');
-const circles = document.querySelectorAll('.circle');
+const more_btn = document.querySelector('.circle-container .more');
+const close_btn = document.querySelector('.circle-container .close');
+const container = document.querySelector('.container');
+const circle_container = document.querySelector('.circle-container');
+const side_nav = document.querySelector('.side-nav');
 
-const totalWidth = document.querySelector('.circles').clientWidth;
-const bar = document.getElementById('progress-bar');
+more_btn.addEventListener('click', rotateContent);
+close_btn.addEventListener('click', restoreRotate);
 
-var count = 1;
-const prevBtn = btns[0];
-const nextBtn = btns[1];
-const total = circles.length;
-const width = circles[0].offsetWidth;
-const dx = (totalWidth - total * width) / (total - 1);
 
-nextBtn.addEventListener('click', () => {
-    const actives = document.querySelectorAll('.active');
-    if(count != total) {
-        setCircleActive();
-        update();
-    }
-    if(count == total - 1) {
-        setNextBtnNoActive();
-    }
-    if(count == 1) {
-        setPrevBtnActive();
-    }
-    if(count < total) ++count;
-    update();
-});
-
-prevBtn.addEventListener('click', () => {
-    if(count == 2) {
-        setPrevBtnNoActive();
-    }
-    if(count > 1) {
-        setCircleNoActive();
-    }
-    if(count == total) {
-        setNextBtnActive();
-    }
-    if(count > 1) count--;
-    update();
-});
-
-function update() {
-    bar.style.width = count * width + (count - 1) * dx + 'px';
+function showOrHideSideNav() {
+    side_nav.classList.toggle('active');
 }
 
-function setCircleActive() {
-    circles[count].classList.add("active");
+function swithBtnStatus() {
+    circle_container.classList.toggle('active');
 }
 
-function setCircleNoActive() {
-    circles[count - 1].classList.remove("active");
+function rotateContent() {
+    container.classList.toggle('active');
+    swithBtnStatus();
+    showOrHideSideNav();
 }
 
-function setNextBtnNoActive() {
-    nextBtn.classList.remove("active-btn");
-}
-
-function setNextBtnActive() {
-    nextBtn.classList.add("active-btn");
-}
-
-function setPrevBtnActive() {
-    prevBtn.classList.add("active-btn");
-}
-
-function setPrevBtnNoActive() {
-    prevBtn.classList.remove("active-btn");
+function restoreRotate() {
+    rotateContent();
 }
